309d3fed0e99a0f5d9ef544cc3796a6641f94e59
Raoulnest <nestorratongalaza1@gmail.com>
2022-05-16 09:17:41 +0300

    Initialisation de mon Projet

---
+++ database/migrations/2022_05_16_061551_create_problemes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProblemesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('problemes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('problemes');
+    }
+}
